c49509f76335b200a63cfddca524cdd1688e515a
zabit <xaclafun1991@gmail.com>
2024-04-11 07:36:27 +0300

    .

--- routes.py
+++ routes.py
@@ -37,7 +37,7 @@ def get_user_list():
     return jsonify(user_list)
 
 
-@app.route('/delete_user/<int:user_id>', methods=['GET'])
+@app.route('/delete_user/<int:user_id>', methods=['DELETE'])
 @jwt_required()
 def delete_user(user_id):
     current_user_id = get_jwt_identity()
@@ -74,3 +74,90 @@ def update_user_info(user_id):
     else:
         return {'message': 'You have not permission'}, 403
 
+
+# _______________________________________________________________________________________________________
+
+
+@app.route('/create_review', methods=['POST'])
+@jwt_required()
+def create_review():
+    current_user = User.query.get(get_jwt_identity())
+    review_data = request.json
+    review = Review(title=review_data['title'], desc=review_data['desc'], user=current_user)
+    db.session.add(review)
+    db.session.commit()
+    return {'message': 'Review created successfully'}
+
+
+@app.route('/review_list', methods=['GET'])
+@jwt_required()
+def review_list():
+    reviews = Review.query.all()
+    rev_list = []
+    for review in reviews:
+        review_data = {
+            'id': review.id,
+            'title': review.title,
+        }
+        rev_list.append(review_data)
+    return jsonify(rev_list)
+
+
+@app.route('/review_detail/<int:review_id>', methods=['GET'])
+@jwt_required()
+def get_review(review_id):
+    review = Review.query.get(review_id)
+    if not review:
+        return {'message': 'Review not found'}, 404
+
+    review_data = {
+        'id': review.id,
+        'title': review.title,
+        'desc': review.desc,
+        'created_at': review.created_at,
+        'user_id': review.user_id
+    }
+    return jsonify(review_data)
+
+
+@app.route('/update_review/<int:review_id>', methods=['PUT'])
+@jwt_required()
+def update_review(review_id):
+    current_user = User.query.get(get_jwt_identity())
+    review = Review.query.get(review_id)
+    if not review:
+        return {'message': 'Review not found'}, 404
+
+    if current_user == review.user or current_user.is_admin:
+        data = request.json
+        if 'title' in data:
+            review.title = data['title']
+        if 'desc' in data:
+            review.desc = data['desc']
+        db.session.commit()
+        updated_review = {
+            'id': review.id,
+            'title': review.title,
+            'desc': review.desc,
+            'created_at': review.created_at,
+            'user_id': review.user_id
+        }
+        return jsonify(updated_review)
+    else:
+        return {'message': 'You have not permission'}, 403
+
+
+@app.route('/delete_review/<int:review_id>', methods=['DELETE'])
+@jwt_required()
+def delete_review(review_id):
+    current_user = User.query.get(get_jwt_identity())
+    review = Review.query.get(review_id)
+    if not review:
+        return {'message': 'Review not found'}, 404
+
+    if current_user == review.user or current_user.is_admin:
+        db.session.delete(review)
+        db.session.commit()
+        return {'message': 'Review deleted successfully'}
+    else:
+        return {'message': 'You have not permission'}, 403
